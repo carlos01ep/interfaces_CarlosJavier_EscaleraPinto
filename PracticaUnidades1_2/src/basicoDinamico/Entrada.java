393ef2d90447b78abda6547298a464d739aafd01
carlos01ep <98832342+carlos01ep@users.noreply.github.com>
2022-10-19 21:08:17 +0200

    diseño2

---
+++ PracticaUnidades1_2/src/basicoDinamico/Entrada.java
@@ -0,0 +1,9 @@
+package basicoDinamico;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Entrada {
+
+
+    
+}
--- PracticaUnidades1_2/src/module-info.java
+++ PracticaUnidades1_2/src/module-info.java
@@ -2,6 +2,7 @@ module PracticaUnidades1_2 {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
